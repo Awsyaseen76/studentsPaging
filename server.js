d6bb15195137ba9b18585338140ae07ba03724a0
Aws <Awsyaseen76@gmail.com>
2018-09-24 19:57:17 +0300

    create the server

---
+++ server.js
@@ -0,0 +1,43 @@
+var express 	 = require('express');
+var app 		 = express();
+var bodyParser 	 = require('body-parser');
+
+// security
+var passport     = require('passport');
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
+var port = process.env.PORT || 8080;
+
+// to add secret variables to the process.env through the .env file which it ignored from gitting to github
+require('dotenv').config();
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+	
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./server/AllUsers/users.service.server')(app);
+require('./server/grades/grades.service.server')(app);
+require('./server/schools/schools.service.server')(app);
+require('./server/databse.js');
+
+
+require('./server/passport.js')(passport);
+
+console.log(port);
+app.listen(port, function() {
+	console.log('SPS connected to: '+port);
+});
